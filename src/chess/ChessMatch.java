65ee198549eaddf7018eb6c2e462dca05a936fe9
Rodrigo Barbosa <rodrigo.dsb93@gmail.com>
2020-12-21 19:40:26 -0300

    check logic

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -8,13 +8,15 @@ import chess.pieces.Rook;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ChessMatch {
     private int turn;
     private Color currentPlayer;
     private Board board;
+    private boolean check;
 
-    private List<Piece> piecesOndTheBoard = new ArrayList<>();
+    private List<Piece> piecesOnTheBoard = new ArrayList<>();
     private List<Piece> capturedPieces = new ArrayList<>();
 
     public ChessMatch(){
@@ -29,6 +31,10 @@ public class ChessMatch {
 
     public Color getCurrentPlayer(){
         return currentPlayer;
+    }
+
+    public boolean getCheck(){
+        return check;
     }
 
     public ChessPiece[][] getPieces(){
@@ -51,8 +57,16 @@ public class ChessMatch {
         Position source = sourcePosition.toPosition();
         Position target = targetPosition.toPosition();
         validateSourcePosition(source);
-        validateTargetPosisiton(source, target);
+        validateTargetPosition(source, target);
         Piece capturedPiece = makeMove(source, target);
+
+        if(testCheck(currentPlayer)){
+            undoMove(source, target, capturedPiece);
+            throw new ChessException("You can't yourself in check");
+        }
+
+        check = (testCheck(opponent(currentPlayer))) ? true: false;
+
         nextTurn();
         return (ChessPiece) capturedPiece;
     }
@@ -62,11 +76,21 @@ public class ChessMatch {
         Piece capturedPiece = board.removePiece(target);
         board.placePiece(p, target);
         if(capturedPiece != null){
-            piecesOndTheBoard.remove(capturedPiece);
+            piecesOnTheBoard.remove(capturedPiece);
             capturedPieces.add(capturedPiece);
         }
 
         return capturedPiece;
+    }
+
+    private void undoMove(Position source, Position target, Piece capturedPiece){
+        Piece p = board.removePiece(target);
+        board.placePiece(p, source);
+        if(capturedPiece != null){
+            board.placePiece(capturedPiece, target);
+            capturedPieces.remove(capturedPiece);
+            piecesOnTheBoard.add(capturedPiece);
+        }
     }
 
     private void validateSourcePosition(Position position){
@@ -81,7 +105,7 @@ public class ChessMatch {
         }
     }
 
-    private void validateTargetPosisiton(Position source, Position target){
+    private void validateTargetPosition(Position source, Position target){
         if(!board.piece(source).possibleMoves(target)){
             throw new ChessException("The chosen piece can't move to target position");
         }
@@ -92,9 +116,35 @@ public class ChessMatch {
         currentPlayer = (currentPlayer == Color.WHITE) ? Color.BLACK: Color.WHITE;
      }
 
+     private Color opponent(Color color){
+        return (color == Color.WHITE) ? Color.BLACK : Color.WHITE;
+     }
+
+    private ChessPiece king(Color color){
+        List<Piece> list = piecesOnTheBoard.stream().filter(x -> ((ChessPiece)x).getColor() == color).collect(Collectors.toList());
+        for(Piece p : list){
+            if(p instanceof King){
+                return (ChessPiece)p;
+            }
+        }
+        throw new IllegalArgumentException("There is no " + color + " king on the board");
+    }
+
+    private boolean testCheck(Color color){
+        Position kingPosition = king(color).getChessPosition().toPosition();
+        List<Piece> opponentPieces = piecesOnTheBoard.stream().filter(x -> ((ChessPiece)x).getColor() == opponent(color)).collect(Collectors.toList());
+        for( Piece p: opponentPieces){
+            boolean[][] mat = p.possibleMoves();
+            if(mat[kingPosition.getRow()][kingPosition.getColumn()]){
+                return true;
+            }
+        }
+        return false;
+    }
+
     private void placeNewPiece(char column, int row, ChessPiece piece){
         board.placePiece(piece, new ChessPosition(column, row).toPosition());
-        piecesOndTheBoard.add(piece);
+        piecesOnTheBoard.add(piece);
     }
 
     private void initialSetup(){
